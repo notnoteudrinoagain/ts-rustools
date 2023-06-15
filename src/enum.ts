ffe2a2c297cba99b7f27d9de459e9d3a73bf4c97
eudrino <eudrinoga@gmail.com>
2023-06-15 21:33:03 +0300

    smaller implementation

--- src/enum.ts
+++ src/enum.ts
@@ -1,24 +1,12 @@
-/**
- * Enums give you a way of saying a value is one of a possible set of values.
- * ```ts
- * const IpAddr = new Enum<{ V4: string; V6: string }>(["V4", "127.0.0.1"]);
- * ```
- */
-class Enum<VS extends Record<string, unknown>> {
-  value: [keyof VS, VS[keyof VS]];
-  constructor([variant, value]: [keyof VS, VS[keyof VS]]) {
-    this.value = [variant, value];
+class Enum<T extends Record<string, unknown>, V extends keyof T = keyof T> {
+  variant: V;
+  value: T[V];
+  constructor(variant: V, value: T[V]) {
+    this.variant = variant;
+    this.value = value;
   }
-  /**
-   *
-   * @param on The key you want to match on.
-   * @param matcher The match body
-   */
-  match_on<I extends keyof VS, V extends VS[I]>(
-    on: I,
-    matcher: (arg0: V) => void
-  ) {
-    if (this.value[0] == on) matcher(this.value[1] as unknown as V);
+  matches<K extends V>(key: K): this is Enum<{ [_ in K]: T[K] }> {
+    return this.variant == key;
   }
 }
 
--- src/result.ts
+++ src/result.ts
@@ -1,64 +1,35 @@
 import Enum from "./enum";
 import Optional, { None, Some } from "./optional";
 
-/**
- * Result is a utility class that represents either success ([`Ok`]) or failure ([`Err`])
- */
 class Result<T, E> extends Enum<{ Ok: T; Err: E }> {
-  constructor(value: ["Ok", T] | ["Err", E]) {
-    super(value);
+  is_ok(): this is Enum<{ Ok: T }> {
+    return this.matches("Ok");
   }
-  /**
-   * Transforms the `Result<T, E>` into an `Optional<T>`.
-   *
-   * mapping `Ok(v)` to `Some(v)`, and `Err` to `None`.
-   */
-  ok(): Optional<T, null> {
-    return this.value[0] == "Ok" ? Some(this.value[1] as T) : None();
+  is_err(): this is Enum<{ Err: E }> {
+    return this.matches("Err");
   }
-}
-/**
- * Instantiates a `Result` with the `Ok` variant.
- * @returns `Result<T, E>`
- * @example
- * ```ts
- * function readFromDB(): Result<number, APIError> {
- *   if (successFullRead) return Ok(data);
- *   else return Err(new APIError("Bad Request"));
- * }
- * let count = readFromDB();
- * count.match_on("Ok", v => console.log(v)); // 2
- * ```
- *
- */
-export function Ok<T, E>(value: T): Result<T, E> {
-  return new Result<T, E>(["Ok", value]);
+  match<R>(f: { Ok: (v: T) => R; Err: (e: E) => R }): R {
+    return f[this.variant](this.value as T & E);
+  }
+  ok(): Optional<T> {
+    return this.match({
+      Ok: (v) => Some(v),
+      Err: () => None(),
+    });
+  }
 }
 
-/**
- * Instantiates a `Result` with the `Err` variant.
- * @returns `Result<T, E>`
- * @example
- * ```ts
- * function readFromDB(): Result<number, APIError> {
- *   if (successFullRead) return Ok(data);
- *   else return Err(new APIError("Bad Request"));
- * }
- * let count = readFromDB();
- * count.match_on("Err", err => console.log(v)); // APIError
- * ```
- *
- */
-export function Err<T, E>(err: E): Result<T, E> {
-  return new Result<T, E>(["Err", err]);
+export function Ok<T, E>(value: T) {
+  return new Result<T, E>("Ok", value);
 }
 
-/**
- * A helper function that turns a fallible function's into a Result safe version.
- */
-export function intoResult<T>(f: () => T): Result<T, string> {
+export function Err<T, E>(err: E) {
+  return new Result<T, E>("Err", err);
+}
+
+export function catchInto<T>(fn: () => T): Result<T, string> {
   try {
-    return Ok(f());
+    return Ok(fn());
   } catch (e) {
     return Err(String(e));
   }
